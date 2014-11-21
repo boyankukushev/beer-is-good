eaa2426071544a344770424647ef95b08863393b
Boyan Kukushev <boyan.kukushev@ontotext.com>
2014-11-21 14:56:09 +0200

    it did begin ...

---
+++ webservices-archetype/README.md
@@ -0,0 +1,5 @@
+webservices-archetype
+======
+
+Archetype for generating spring-aware web applications with swagger and healtchecks
+
---
+++ webservices-archetype/src/main/resources/archetype-resources/src/main/java/com/ontotext/swagger/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.ontotext.swagger;
+ 
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+ 
+@EnableSwagger
+@Configuration
+public class SwaggerConfiguration {
+    @Inject
+    private SpringSwaggerConfig springSwaggerConfig;
+ 
+
+    @Value("${documentation.services.version}")
+    private String apiVersion;
+    
+    @Bean
+    public SwaggerSpringMvcPlugin createSwaggerPlugin() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiVersion(this.apiVersion)
+                .apiInfo(new ApiInfo( // TODO: change the fields below as appropriate
+                        "Webservice API",
+                        "Web Application for Doing Stuff",
+                        null, // terms of service url
+                        null, // contact
+                        null, // license
+                        null // license url
+                ));
+    }
+}
